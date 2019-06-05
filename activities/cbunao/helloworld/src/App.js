dc0bfff458b8f20f7bea6f351be5ac3c5fdc17a1
kobe <kobearelativo@gmail.com>
2019-06-05 22:49:30 +0800

    keep origin up to date

---
+++ activities/cbunao/helloworld/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World, I am Chienih Louise Bunao
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <img src="https://adobochroniclesdotcom.files.wordpress.com/2018/11/b13f4fda-e6a8-41e1-ac1f-3b2fa4efabf2.jpeg?w=474&h=267"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
