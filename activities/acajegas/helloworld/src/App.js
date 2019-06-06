1807cad6b56eae17f905295988acb12a85d2ce85
jruiz123 <artjohncajegas@gmail.com>
2019-06-06 19:13:36 +0800

    Activity 2 - create components

--- activities/acajegas/helloworld/src/App.js
+++ activities/acajegas/helloworld/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './tenor.gif';
 import './App.css';
+import Header from './header.js';
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! I am AJC!
+          Ang Pambansang Ibon ng Pilipinas!
         </p>
+        <Header />
       </header>
     </div>
   );
