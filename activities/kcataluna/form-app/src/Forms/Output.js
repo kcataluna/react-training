95c2a86a4530b7faa11c652e68afebebe7ba3493
Keven Client A. Cataluña <soodokod@gmail.com>
2019-06-08 13:43:26 +0800

    disable input field of output form

--- activities/kcataluna/form-app/src/Forms/Output.js
+++ activities/kcataluna/form-app/src/Forms/Output.js
@@ -7,11 +7,13 @@ class Output extends React.Component {
                 <h5 className="card-header">Output</h5>
                 <div className="card-body">
                     <form action="javascript:void(0)">
-                        <img src="https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png" className="img-fluid form-group" alt="" />
-                        <div className="form-group">
-                            <label htmlFor="inputID">ID Number</label>
-                            <input type="text" className="form-control" id="inputID" placeholder="" />
-                        </div>
+                        <fieldset disabled>
+                            <img src="https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png" className="img-fluid form-group" alt="" />
+                            <div className="form-group">
+                                <label htmlFor="inputID">ID Number</label>
+                                <input type="text" className="form-control" id="inputID" placeholder="" />
+                            </div>
+                        </fieldset>
                     </form>
                 </div>
             </div>
