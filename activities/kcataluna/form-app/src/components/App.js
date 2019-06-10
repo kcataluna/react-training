cd35105fb55b69dfb0508713764d71a574e2d630
Keven Client A. Cataluña <kcataluna@csyoutsourcing.com>
2019-06-10 17:36:19 +0800

    Add bs-custom-file-input

--- activities/kcataluna/form-app/src/components/App.js
+++ activities/kcataluna/form-app/src/components/App.js
@@ -5,16 +5,23 @@ import './App.css';
 import Clock from '../components/Clock/';
 import Forms from '../components/Forms/';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Clock />
-                <Forms />
-            </header>
-        </div>
-    );
+import bsCustomFileInput from 'bs-custom-file-input';
+
+class App extends React.Component {
+    componentDidMount() {
+        bsCustomFileInput.init();
+    }
+    render() {
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <Clock />
+                    <Forms />
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
