5cef4b2574d98114ffb08885b09f9f78eb9708b7
Keven Client A. Cataluña <kcataluna@csyoutsourcing.com>
2019-06-11 09:32:14 +0800

    Remove onSubmit to onChange

--- activities/kcataluna/form-app/src/containers/Input.js
+++ activities/kcataluna/form-app/src/containers/Input.js
@@ -1,63 +1,75 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { setCredentials } from '../actions';
+import {connect} from 'react-redux';
+import {setCredentials} from '../actions';
+
+const input = {
+    id: 'inputID',
+    name: 'inputName',
+    birthdate: 'inputBirthdate',
+    address: 'inputAddress',
+    picture: 'filePicture'
+};
 
 const Input = ({ dispatch }) => {
     let id, name, birthdate, address, picture;
-    const dispatcher = credentials => {
-        dispatch(setCredentials(credentials));
-        id.value = name.value = birthdate.value = address.value = '';
+    const handleChange = event => {
+        if(event.target.id == input.picture) {
+            let reader = new FileReader(), file = event.target.files[0];
+            try {
+                reader.onloadend = () => {
+                    picture = reader.result;
+                    dispatch(setCredentials({id, name, birthdate, address, picture}));
+                }
+                reader.readAsDataURL(file);
+            } catch (error) {
+                picture = null;
+                dispatch(setCredentials({id, name, birthdate, address, picture}));
+            }
+            return;
+        } else {
+            switch(event.target.id) {
+                case input.id: 
+                    id = event.target.value; 
+                    break;
+                case input.name: 
+                    name = event.target.value; 
+                    break;
+                case input.birthdate: 
+                    birthdate = event.target.value; 
+                    break;
+                case input.address: 
+                    address = event.target.value;
+                    break;   
+                default: break;
+            }
+            dispatch(setCredentials({id, name, birthdate, address, picture}));
+        }
     }
     return (
         <div className="card mb-3">
             <h5 className="card-header">Input</h5>
             <div className="card-body">
-                <form onSubmit={
-                    e => {
-                        e.preventDefault();
-
-                        let reader = new FileReader(), file = picture.files[0],
-                            obj = {
-                                id: id.value,
-                                name: name.value,
-                                birthdate: birthdate.value,
-                                address: address.value,
-                                picture: reader.result
-                            };
-                        
-                        try {
-                            reader.onloadend = () => {
-                                obj.picture = reader.result;
-                                dispatcher(obj);
-                            }
-                            reader.readAsDataURL(file);
-                        } catch(error) {
-                            obj.picture = null;
-                            dispatcher(obj);
-                        }
-                    }
-                }>
+                <form action='javascript:void(0)'>
                     <div className="form-group">
                         <label htmlFor="inputID">ID Number</label>
-                        <input type="text" className="form-control" id="inputID" placeholder="" ref={input => id = input} />
+                        <input type="text" className="form-control" id={input.id} placeholder="" onChange={handleChange} />
                     </div>
                      <div className="form-group">
                         <label htmlFor="inputName">Name</label>
-                        <input type="text" className="form-control" id="inputName" placeholder="" ref={input => name = input} />
+                        <input type="text" className="form-control" id={input.name} placeholder="" onChange={handleChange} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputBirthdate">Birthdate</label>
-                        <input type="text" className="form-control" id="inputBirthdate" placeholder="" ref={input => birthdate = input} />
+                        <input type="text" className="form-control" id={input.birthdate} placeholder="" onChange={handleChange} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputAddress">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="" ref={input => address = input} />
+                        <input type="text" className="form-control" id={input.address} placeholder="" onChange={handleChange} />
                     </div>
                     <div className="custom-file">
-                        <input type="file" className="custom-file-input" id="filePicture" ref={input => picture = input} />
+                        <input type="file" className="custom-file-input" id={input.picture} onChange={handleChange} />
                         <label className="custom-file-label" htmlFor="filePicture">Choose file</label>
                     </div>
-                    <button className="btn btn-primary btn-block mt-3 float-right" type="submit">Submit</button>
                 </form>
             </div>
         </div>
