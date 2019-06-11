be343d5fe6d4230bff05044ff8680e61651b0496
Keven Client A. Cataluña <kcataluna@csyoutsourcing.com>
2019-06-11 09:42:57 +0800

    organize output form

--- activities/kcataluna/form-app/src/components/Forms/Output.js
+++ activities/kcataluna/form-app/src/components/Forms/Output.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
-const Output = ({credentials}) => (
-    <div className="card mb-3">
-        <h5 className="card-header">Output</h5>
-        <div className="card-body">
-            <form action="javascript:void(0)">
-                <fieldset disabled>
-                    <img src={credentials.picture} className="img-fluid form-group" alt="" />
-                    <div className="form-group">
-                        <label htmlFor="inputID">ID Number</label>
-                        <input type="text" className="form-control" id="inputID" placeholder="" defaultValue={credentials.id} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputName">Name</label>
-                        <input type="text" className="form-control" id="inputName" placeholder="" defaultValue={credentials.name} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputBirthdate">Birthdate</label>
-                        <input type="text" className="form-control" id="inputBirthdate" placeholder="" defaultValue={credentials.birthdate} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputAddress">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="" defaultValue={credentials.address} />
-                    </div>
-                </fieldset>
-            </form>
+const Output = ({credentials}) => {
+    let imageClass = credentials.picture ? 'img-fluid form-group' : 'd-none';
+    return (
+        <div className="card mb-3">
+            <h5 className="card-header">Output</h5>
+            <div className="card-body">
+                <form action="javascript:void(0)">
+                    <fieldset disabled>
+                        <img src={credentials.picture} className={imageClass} alt="" />
+                        <div className="form-group">
+                            <label htmlFor="inputID">ID Number</label>
+                            <input type="text" className="form-control" placeholder="" defaultValue={credentials.id} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="inputName">Name</label>
+                            <input type="text" className="form-control" placeholder="" defaultValue={credentials.name} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="inputBirthdate">Birthdate</label>
+                            <input type="text" className="form-control" placeholder="" defaultValue={credentials.birthdate} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="inputAddress">Address</label>
+                            <input type="text" className="form-control" placeholder="" defaultValue={credentials.address} />
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Output;
