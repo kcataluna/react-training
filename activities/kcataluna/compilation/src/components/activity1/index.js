f8a953cb39cd3e3ad1d5b0a98d7ed4a457585c79
Keven Client A. Cataluña <kcataluna@csyoutsourcing.com>
2019-06-13 16:34:31 +0800

    Isolate default fontsize to HelloWorld only

--- activities/kcataluna/compilation/src/components/activity1/index.js
+++ activities/kcataluna/compilation/src/components/activity1/index.js
@@ -4,8 +4,9 @@ import logo from '../logo.svg';
 
 class HelloWorld extends React.Component {
     render = () => {
+        const style = {fontSize: 'calc(10px + 2vmin)'};
         return (
-            <div className="text-center">
+            <div className="text-center" style={style}>
                 <img src={logo} className="App-logo-lg" alt="logo" />
                 <p>Hello World, I am <code>Keven Client</code> !</p>
                 <p className="App-link">hajimemashite</p>
