f2cb8302e19c1d9e64fa54cfb149610ea0b52a05
Keven Client A. Cataluña <kcataluna@csyoutsourcing.com>
2019-06-14 16:41:22 +0800

    defaultValue to value

--- activities/kcataluna/compilation/src/components/activity3/Output.js
+++ activities/kcataluna/compilation/src/components/activity3/Output.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 class Output extends React.Component {
-    render = () => {
+    render = () => {console.log(this.props.id);
         let imageClass = this.props.picture ? 'img-fluid form-group' : 'd-none';
         return (
             <div className="card mb-3">
@@ -13,19 +13,19 @@ class Output extends React.Component {
                             <img src={this.props.picture} className={imageClass} alt="" />
                             <div className="form-group">
                                 <label htmlFor="inputID">ID Number</label>
-                                <input type="text" className="form-control" placeholder="" defaultValue={this.props.id} />
+                                <input type="text" className="form-control" placeholder="" value={this.props.id} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="inputName">Name</label>
-                                <input type="text" className="form-control" placeholder="" defaultValue={this.props.name} />
+                                <input type="text" className="form-control" placeholder="" value={this.props.name} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="inputBirthdate">Birthdate</label>
-                                <input type="text" className="form-control" placeholder="" defaultValue={this.props.birthdate} />
+                                <input type="text" className="form-control" placeholder="" value={this.props.birthdate} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="inputAddress">Address</label>
-                                <input type="text" className="form-control" placeholder="" defaultValue={this.props.address} />
+                                <input type="text" className="form-control" placeholder="" value={this.props.address} />
                             </div>
                         </fieldset>
                     </form>
